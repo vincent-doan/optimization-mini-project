fb60e106d4ef1c537840d2536c24d6f86651d251
vincent-doan <vinh.dt210940@sis.hust.edu.vn>
2023-02-03 21:56:07 +0700

    modularized

--- generate_input.py
+++ generate_input.py
@@ -1,23 +1,19 @@
-import random
-
-def generate(N, M, seed_value):
+def generate_input(N, M, seed_value):
+    import random
     lines = list()
     lines.append(str(N) + ' ' + str(M))
     random.seed(seed_value)
     for _ in range(N):
         t = random.randint(2, 4)
-        g = random.randint(1, round(N/5))
-        s = random.randint(30,150)
+        g = random.randint(1, round(N/6))
+        s = random.choice([30 + 10*i for i in range(13)])
         lines.append(str(t) + ' ' + str(g) + ' ' + str(s))
     c = ''
     for _ in range(M):
-        c += str(random.choice([60, 100, 140, 180])) + ' '
+        c += str(random.choice([60, 80, 100, 120, 140, 160, 180])) + ' '
     lines.append(c)
-    return lines
-
-lines = generate(100, 10, 1)
-with open("input.txt", 'w') as f:
-    for index, line in enumerate(lines):
-        f.write(line)
-        if index == len(lines) - 1: continue
-        f.write('\n') 
+    with open("input.txt", 'w') as f:
+        for index, line in enumerate(lines):
+            f.write(line)
+            if index == len(lines) - 1: continue
+            f.write('\n') 
